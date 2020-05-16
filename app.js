0e3448a17136e7b9b01489597b0bd33f7cf58646
Abhayks2808 <Abhayks2808@gmail.com>
2020-05-16 22:58:13 +0530

    aj

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const     session=require('express-session')
 const     Campground=require("./models/campground")
 const      Comment=require("./models/comment")
 const      User=require('./models/user')
-const      port=process.env.port || 3000;
+const      port=process.env.PORT || 3000;
       
      
       
